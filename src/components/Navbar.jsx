6472599e4d07cdde48cd21ec2e00886ebbaf7ac4
gatere <mercynjerigatere@gmail.com>
2024-05-09 11:17:24 +0300

    Hover name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,11 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-4">Metro News</span></a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      
+      <button class="button" data-text="Awesome">
+    <span class="actual-text">&nbsp;MetroNews&nbsp;</span>
+    <span aria-hidden="true" class="hover-text">&nbsp;MetroNews&nbsp;</span>
+</button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
