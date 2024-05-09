9ad806a5a0b775220bf902d5fe794f6e085cf822
gatere <mercynjerigatere@gmail.com>
2024-05-09 11:41:05 +0300

    Adds more changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'boxicons'
-import 'semantic-ui-css/semantic.min.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
